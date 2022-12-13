c54c0ee2d2a17045d11b7a44b8df344345fb8b76
Pavel Demin <pavel-demin@outlook.com>
2022-12-13 17:33:04 +0100

    update ZmodADC

--- ZmodADC.c
+++ ZmodADC.c
@@ -171,6 +171,74 @@ FDisplayZmodADCCal(int fdI2cDev, BYTE addrI2cSlave) {
 }
 
 /* ------------------------------------------------------------ */
+/***    FGetZmodADCCal
+**
+**  Parameters:
+**  	fdI2cDev        - open file descriptor for underlying I2C device (linux only)
+**      addrI2cSlave    - I2C bus address for the slave
+**      pFactoryCal			- ZMOD_ADC_CAL object to return factory calibration data through
+**      pUserCal			- ZMOD_ADC_CAL object to return user calibration data through
+**
+**  Return Value:
+**      fTrue for success, fFalse otherwise
+**
+**  Errors:
+**      none
+**
+**  Description:
+**      This function reads the factory calibration and user calibration
+**      areas from the ZmodADC with the specified I2C bus address,
+**      then returns the calibration data by argument.
+*/
+BOOL
+FGetZmodADCCal(int fdI2cDev, BYTE addrI2cSlave, ZMOD_ADC_CAL *pFactoryCal, ZMOD_ADC_CAL *pUserCal) {
+
+    WORD            cbRead;
+
+    if ( ! SyzygyI2cRead(fdI2cDev, addrI2cSlave, addrAdcFactCalStart, (BYTE*)pFactoryCal, sizeof(ZMOD_ADC_CAL), &cbRead) ) {
+        printf("Error: failed to read ZmodADC factory calibration from 0x%02X\n", addrI2cSlave);
+        printf("Error: received %d of %d bytes\n", cbRead, sizeof(ZMOD_ADC_CAL));
+        return fFalse;
+    }
+
+    if ( ! SyzygyI2cRead(fdI2cDev, addrI2cSlave, addrAdcUserCalStart, (BYTE*)pUserCal, sizeof(ZMOD_ADC_CAL), &cbRead) ) {
+        printf("Error: failed to read ZmodADC user calibration from 0x%02X\n", addrI2cSlave);
+        printf("Error: received %d of %d bytes\n", cbRead, sizeof(ZMOD_ADC_CAL));
+        return fFalse;
+    }
+
+    return fTrue;
+}
+
+/* ------------------------------------------------------------ */
+/***    FZmodADCCalConvertToS18
+**
+**  Parameters:
+**      adcal			- ZMOD_ADC_CAL object to pull calibration coefficients from
+**      pReturn			- ZMOD_ADC_CAL_S18 object to return data by argument
+**
+**  Return Value:
+**      none
+**
+**  Errors:
+**      none
+**
+**  Description:
+**      This function converts calibration coefficients to the 18-bit signed format used in PL hardware.
+*/
+void
+FZmodADCCalConvertToS18(ZMOD_ADC_CAL adcal, ZMOD_ADC_CAL_S18 *pReturn) {
+	pReturn->cal[0][0][0] = (unsigned int)ComputeMultCoefADC1410(adcal.cal[0][0][0], fFalse);
+	pReturn->cal[0][0][1] = (unsigned int)ComputeAddCoefADC1410(adcal.cal[0][0][1], fFalse);
+	pReturn->cal[0][1][0] = (unsigned int)ComputeMultCoefADC1410(adcal.cal[0][1][0], fTrue);
+	pReturn->cal[0][1][1] = (unsigned int)ComputeAddCoefADC1410(adcal.cal[0][1][1], fTrue);
+	pReturn->cal[1][0][0] = (unsigned int)ComputeMultCoefADC1410(adcal.cal[1][0][0], fFalse);
+	pReturn->cal[1][0][1] = (unsigned int)ComputeAddCoefADC1410(adcal.cal[1][0][1], fFalse);
+	pReturn->cal[1][1][0] = (unsigned int)ComputeMultCoefADC1410(adcal.cal[1][1][0], fTrue);
+	pReturn->cal[1][1][1] = (unsigned int)ComputeAddCoefADC1410(adcal.cal[1][1][1], fTrue);
+}
+
+/* ------------------------------------------------------------ */
 /***    ComputeMultCoefADC1410
 **
 **  Parameters:
